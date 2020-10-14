a316df050fcf070e02127ab2ff9afd8fb691dc08
William Borely <69053982+williamborely@users.noreply.github.com>
2020-10-14 17:01:09 -0300

    Projeto To Do List
    Projeto feito em duas partes, backend para utilizar o mongodb para guardar informações e frontend para projetar toda a parte da aparência e funções do To Do App.

---
+++ todolist/frontend/src/about/about.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PageHeader from '../template/pageHeader';
+
+export default (props) => (
+    <div>
+        <PageHeader name="Sobre" small="Nós"/>
+
+        <h2>Nossa Histórioa</h2>
+        <p>Lorem Ipsum Dolor Sit Amet ...</p>
+
+        <h2>Missão e Visão</h2>
+        <p>Lorem Ipsum Dolor Sit Amet ...</p>
+
+        <h2>Empresa</h2>
+        <p>Lorem Ipsum Dolor Sit Amet ...</p>
+    </div>
+)
